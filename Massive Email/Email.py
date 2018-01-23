393629e16f1aadc087bd15aa4e6b062f29155a01
reponic tannous <reponic340@gmail.com>
2018-01-23 14:43:42 -0500

    Changing the data

--- Massive Email/Email.py
+++ Massive Email/Email.py
@@ -1,14 +1,14 @@
 import smtplib
-fromaddr = 'testemail@gmail.com'
-toaddrs  = 'othertestemail@decoariescorp.com'
+fromaddr = 'test"gmail.com'
+toaddrs  = 'testOther@gmail.com'
 msg = "\r\n".join([
-  "From: testemail@gmail.com",
-  "To: othertestemail@gmail.com",
-  "Subject: Just a message",
+  "From: test@gmail.com",
+  "To: testOther@gmail.com",
+  "Subject: Test",
   "",
-  "Why, oh why"
+  "test"
   ])
-username = 'testemail@gmail.com'
+username = 'test@gmail.com'
 password = 'password'
 server = smtplib.SMTP('smtp.gmail.com:587')
 server.ehlo()
